d044005dbae006c24e73f8dbe935e671ecd3e30e
Vago <officiallovemen@gmail.com>
2026-05-31 10:10:01 +0300

    Ваше описание изменений

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -13,7 +13,32 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg",
+];
+
+const slider = document.getElementById("web-tech-image");
+const buttonPrev = document.getElementById("prev-button");
+const buttonNext = document.getElementById("next-button");
+let currentIndex = 0;
+
+buttonPrev.addEventListener("click", () => {
+  currentIndex--;
+
+  if (currentIndex < 0) {
+    currentIndex = WEB_TECH_IMAGES.length - 1;
+  }
+
+  slider.src = WEB_TECH_IMAGES[currentIndex];
+});
+
+buttonNext.addEventListener("click", () => {
+  currentIndex++;
+
+  if (currentIndex >= WEB_TECH_IMAGES.length) {
+    currentIndex = 0;
+  }
+
+  slider.src = WEB_TECH_IMAGES[currentIndex];
+});
